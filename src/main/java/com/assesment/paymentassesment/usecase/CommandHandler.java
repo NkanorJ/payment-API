6f5751aa5d3e7b5d28768818a047f800fc0f7797
Jane Etim <janee@aku.africa>
2023-12-18 10:42:31 +0100

    Removed unused classes

--- src/main/java/com/assesment/paymentassesment/usecase/CommandHandler.java
+++
@@ -1,7 +0,0 @@
-package com.assesment.paymentassesment.usecase;
-
-import com.assesment.paymentassesment.dto.Response;
-
-public interface CommandHandler<T, T1> {
-    Response handle(Void unused);
-}
